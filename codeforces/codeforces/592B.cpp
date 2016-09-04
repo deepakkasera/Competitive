9f082c43b45649d539f0571a9dd12f0f6e91e4c8
deepakkasera <deepak.kasera143.dk@gmail.com>
2016-09-04 17:08:47 +0530

    codeforces problem

--- codeforces/codeforces/592B.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	long long int n;
-	cin>>n;
-	long long int ans=(n-2)*(n-2);
-	cout<<ans;
-	return 0;
-}
--- codeforces/codeforces/678A.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	int n,x;
-	cin>>n>>x;
-	int rem=n%x;
-	cout<<n+(x-rem);
-	return 0;	
-	}
